df52412faa5b901c135734231921a63e00bb4158
peter <petersp2000@gmail.com>
2023-04-16 18:50:33 +0100

    implemented log parser

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
 a script that reads stdin line by line and computes metrics:
+Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size> (if the format is not this one,
+the line must be skipped)
 """
 import re
 
